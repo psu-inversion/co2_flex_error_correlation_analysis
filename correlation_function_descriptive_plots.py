b0d05e9edd4a2d4a4d409c63aa8b9de3e68bf19d
DWesl <22566757+DWesl@users.noreply.github.com>
2020-07-13 15:08:40 -0400

    Drop plot I no longer use.

--- correlation_function_descriptive_plots.py
+++ correlation_function_descriptive_plots.py
@@ -73,43 +73,6 @@ fig.tight_layout()
 fig.savefig("demonstration-setup.pdf")
 plt.close(fig)
 
-# Describe the slots
-fig, axes = plt.subplots(
-    3, 2, figsize=(AX_WIDTH, AX_HEIGHT), sharex=True, sharey=True
-)
-
-axes[0, 0].plot(TIMES, np.cos(2 * np.pi * TIMES / DAYS_PER_DAY))
-axes[0, 0].set_ylabel("Daily Cycle")
-axes[0, 1].plot(TIMES, np.exp(-(np.sin(2 * np.pi * TIMES / DAYS_PER_DAY) / 0.5) ** 2))
-
-axes[2, 0].plot(TIMES, np.cos(2 * np.pi * TIMES / DAYS_PER_YEAR))
-axes[2, 0].set_ylabel("Annual Cycle")
-axes[2, 1].plot(TIMES, np.exp(-(np.sin(np.pi * TIMES / DAYS_PER_YEAR) / 0.5) ** 2))
-
-axes[1, 0].plot(
-    TIMES,
-    np.cos(2 * np.pi * TIMES / DAYS_PER_DAY) *
-    (0.5 + 0.5 * np.cos(2 * np.pi * TIMES / DAYS_PER_YEAR))
-)
-axes[1, 0].set_ylabel("Modulation of\ndaily cycle")
-axes[1, 1].plot(
-    TIMES,
-    np.cos(2 * np.pi * TIMES / DAYS_PER_DAY) *
-    np.exp(-(np.sin(np.pi * TIMES / DAYS_PER_YEAR) / 0.5) ** 2)
-)
-
-axes[2, 0].set_xlim(0, DAYS_PER_YEAR)
-axes[2, 0].set_ylim(-1, 1)
-axes[2, 0].set_xticks(np.arange(0, DAYS_PER_YEAR + EPS, 3))
-axes[2, 0].set_xticks(np.arange(0, DAYS_PER_YEAR + EPS, 1), minor=True)
-axes[2, 0].set_xlabel("Lag Time (days)")
-axes[2, 1].set_xlabel("Lag Time (days)")
-
-fig.suptitle("Correlations in errors in:")
-fig.subplots_adjust(left=.15, bottom=.15)
-fig.savefig("demonstration-corr-fun-slots.pdf")
-plt.close(fig)
-
 fig, axes = plt.subplots(
     len(correlation_function_fits.PartForm),
     len(correlation_function_fits.CorrelationPart),
